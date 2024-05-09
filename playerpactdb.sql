450554bd26b6657c922866535e2cbe446a6b1c6c
defintNull / Lorenzo + D'Angelo <lorenzo.dangelo.mail@gmail.com>
2024-05-09 18:33:19 +0200

    dump db

---
+++ playerpactdb.sql
@@ -0,0 +1,399 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Creato il: Mag 09, 2024 alle 18:31
+-- Versione del server: 10.4.32-MariaDB
+-- Versione PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `playerpactdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(32) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `surname` varchar(64) NOT NULL,
+  `birthDate` date NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `image` blob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `chat`
+--
+
+CREATE TABLE `chat` (
+  `id` int(11) NOT NULL,
+  `idpostteam` int(11) NOT NULL,
+  `idpostsell` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `chatuser`
+--
+
+CREATE TABLE `chatuser` (
+  `idchat` int(11) NOT NULL,
+  `iduser` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `comment`
+--
+
+CREATE TABLE `comment` (
+  `id` int(11) NOT NULL,
+  `idpoststandard` int(11) NOT NULL,
+  `iduser` int(11) NOT NULL,
+  `description` varchar(256) NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `message`
+--
+
+CREATE TABLE `message` (
+  `id` int(11) NOT NULL,
+  `idchat` int(11) NOT NULL,
+  `iduser` int(11) NOT NULL,
+  `description` varchar(256) NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `mod`
+--
+
+CREATE TABLE `mod` (
+  `id` int(11) NOT NULL,
+  `username` varchar(32) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `surname` varchar(64) NOT NULL,
+  `birthDate` date NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `image` blob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `moderationcomment`
+--
+
+CREATE TABLE `moderationcomment` (
+  `id` int(11) NOT NULL,
+  `idreport` int(11) NOT NULL,
+  `idmod` int(11) NOT NULL,
+  `description` varchar(256) NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `moderationpost`
+--
+
+CREATE TABLE `moderationpost` (
+  `id` int(11) NOT NULL,
+  `idreport` int(11) NOT NULL,
+  `idmod` int(11) NOT NULL,
+  `description` varchar(256) NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `moderationuser`
+--
+
+CREATE TABLE `moderationuser` (
+  `id` int(11) NOT NULL,
+  `idreport` int(11) NOT NULL,
+  `idmod` int(11) NOT NULL,
+  `description` varchar(256) NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `postsell`
+--
+
+CREATE TABLE `postsell` (
+  `id` int(11) NOT NULL,
+  `title` varchar(32) NOT NULL,
+  `description` varchar(256) NOT NULL,
+  `date` date NOT NULL,
+  `price` float NOT NULL,
+  `image` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `poststandard`
+--
+
+CREATE TABLE `poststandard` (
+  `id` int(11) NOT NULL,
+  `title` varchar(32) NOT NULL,
+  `description` varchar(256) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `postteam`
+--
+
+CREATE TABLE `postteam` (
+  `id` int(11) NOT NULL,
+  `title` varchar(32) NOT NULL,
+  `description` varchar(256) NOT NULL,
+  `date` date NOT NULL,
+  `nMaxPlayer` int(11) NOT NULL,
+  `nPlayers` int(11) NOT NULL,
+  `time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `report`
+--
+
+CREATE TABLE `report` (
+  `id` int(11) NOT NULL,
+  `iduser` int(11) NOT NULL,
+  `idpost` int(11) NOT NULL,
+  `idcomment` int(11) NOT NULL,
+  `type` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL,
+  `description` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(32) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `surname` varchar(64) NOT NULL,
+  `birthDate` date NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `image` blob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indici per le tabelle `chat`
+--
+ALTER TABLE `chat`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `chatuser`
+--
+ALTER TABLE `chatuser`
+  ADD PRIMARY KEY (`idchat`,`iduser`);
+
+--
+-- Indici per le tabelle `comment`
+--
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`id`,`idpoststandard`);
+
+--
+-- Indici per le tabelle `message`
+--
+ALTER TABLE `message`
+  ADD PRIMARY KEY (`id`,`idchat`);
+
+--
+-- Indici per le tabelle `mod`
+--
+ALTER TABLE `mod`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indici per le tabelle `moderationcomment`
+--
+ALTER TABLE `moderationcomment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `moderationpost`
+--
+ALTER TABLE `moderationpost`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `moderationuser`
+--
+ALTER TABLE `moderationuser`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `postsell`
+--
+ALTER TABLE `postsell`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `poststandard`
+--
+ALTER TABLE `poststandard`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `postteam`
+--
+ALTER TABLE `postteam`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `report`
+--
+ALTER TABLE `report`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `chat`
+--
+ALTER TABLE `chat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT per la tabella `comment`
+--
+ALTER TABLE `comment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `mod`
+--
+ALTER TABLE `mod`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `moderationpost`
+--
+ALTER TABLE `moderationpost`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `moderationuser`
+--
+ALTER TABLE `moderationuser`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `postsell`
+--
+ALTER TABLE `postsell`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `poststandard`
+--
+ALTER TABLE `poststandard`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `postteam`
+--
+ALTER TABLE `postteam`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `report`
+--
+ALTER TABLE `report`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
