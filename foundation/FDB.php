00da4102a6838b8858a895eb88b0611f50e44b3d
defintNull / Lorenzo + D'Angelo <lorenzo.dangelo.mail@gmail.com>
2024-05-11 18:06:11 +0200

    store and load db

--- foundation/FDB.php
+++ foundation/FDB.php
@@ -1,9 +1,28 @@
 <?php
+
+use Dotenv\Parser\Value;
+
     require realpath($_SERVER["DOCUMENT_ROOT"]."/envloader.php");
 
     class FDB {
+        private static $instance;
+        private static array $tables = array(
+            "Admin"=>"admin",
+            "Chat"=>"chat",
+            "ChatUser"=>"chatuser",
+            "Comment"=>"comment",
+            "Message"=>"message",
+            "Mod"=>"mod",
+            "ModerationComment"=>"moderationcomment",
+            "ModerationPost"=>"moderationpost",
+            "ModerationUser"=>"moderationuser",
+            "PostSell"=>"postsell",
+            "PostStandard"=>"poststandard",
+            "PostTeam"=>"postteam",
+            "Report"=>"report",
+            "User"=>"user");
+
         private $db;
-        private $instance;
 
         private function __construct() {
 
@@ -15,12 +34,11 @@
 
         }
 
-        function getInstance() {
-
-        }
-
-        function connect() {
-
+        static function getInstance() : FDB {
+            if(self::$instance == null) {
+                self::$instance = new FDB();
+            }
+            return self::$instance;
         }
 
         function query() {
@@ -28,19 +46,53 @@
         }
 
         function close() {
-
+            self::$instance == null;
         }
 
-        function store() {
+        function store(string $class, $entity) : void{
+            $this->db->beginTransaction();
 
+            $sql = "INSERT INTO ". self::$tables[$class]."(";
+            
+            $counter = 0;
+            $values = $entity->getValues();
+            foreach($values as $attrib=>$data) {
+                $sql .= $attrib;
+                if($counter < count($values)-1) {
+                    $sql .= ",";
+                }
+                $counter++;
+            }
+            $sql .= ") VALUES (";
+
+            $counter = 0;
+            foreach($values as $attrib=>$data) {
+                $sql .= $data;
+                if($counter < count($values)-1) {
+                    $sql .= ",";
+                }
+                $counter++;
+            }
+            $sql .= ")";
+
+            $this->db->exec($sql);
+            $this->db->commit();
         }
 
-        function load() {
+        function load(string $class, int $id) : array{
 
+            $sql = "SELECT * FROM ". self::$tables[$class] ." WHERE id=". $id;
+            
+            $sth = $this->db->prepare($sql);
+            $sth->execute();
+
+            $result = $sth->fetch(PDO::FETCH_ASSOC);
+            
+            return $result;
         }
 
         function delete() {
-
+            
         }
 
         function update() {
