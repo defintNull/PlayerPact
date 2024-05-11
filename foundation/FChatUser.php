95fb92bd06443dc76111926df646184ade602812
Ghigolini <45860903+Ghigolini@users.noreply.github.com>
2024-05-11 17:26:51 +0200

    Create FChatUser.php

---
+++ foundation/FChatUser.php
@@ -0,0 +1,23 @@
+<?php
+    class FChatUser implements FDB{
+        function store() {
+
+        }
+
+        function load() {
+
+        }
+
+        function delete() {
+
+        }
+
+        function update() {
+
+        }
+
+        function exists() {
+            
+        }
+    }
+?>
