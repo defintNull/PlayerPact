cc240b28b02ba8945f1b84c3935aa66af887949f
defintNull / Lorenzo + D'Angelo <lorenzo.dangelo.mail@gmail.com>
2024-05-13 18:06:15 +0200

    persistentmanager

--- foundation/FPersistentManager.php
+++ foundation/FPersistentManager.php
@@ -1,5 +1,41 @@
 <?php
     class FPersistentManager {
+
+        function store($obj) {
+            $FClass = self::classConvert($obj);
+            $entity = new $FClass();
+            $entity->store($obj);
+        }
+
+        function load($EClass,$condition) {
+            $FClass = "F".substr($EClass,1); 
+            $entity = new $FClass();
+            $entity->load($condition);
+        }
+
+        function delete($EClass,$condition) {
+            $FClass = "F".substr($EClass,1); 
+            $entity = new $FClass();
+            $entity->delete($condition);
+        }
+
+        function update($obj,$condition) {
+            $FClass = self::classConvert($obj);
+            $entity = new $FClass();
+            $entity->update($obj);
+        }
+
+        function exists($obj) {
+            $FClass = self::classConvert($obj);
+            $entity = new $FClass();
+            $entity->exist($obj);
+        }
+
+        private static function classConvert($obj) : string {
+            $EClass = get_class($obj);
+            $FClass = "F".substr($EClass,1);
+            return $FClass;
+        }
         
     }
 ?>
