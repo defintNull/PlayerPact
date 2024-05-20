c8f3e3a2027ba60dc8213577c4722d133df78c7a
Ghigolini <45860903+Ghigolini@users.noreply.github.com>
2024-05-20 20:29:04 +0200

    Update autoscroll.js

--- resources/js/autoscroll.js
+++ resources/js/autoscroll.js
@@ -90,13 +90,13 @@ function addrows(rows,type) {
         if(type == 'standard') {
             node.outerHTML = 
             '<div class="row post-item" id='+row.id+'>'+
+            '<div class="description">'+row.iduser+'</div>'+
             '<div class="row title-post-bar">' +
             '<div class="col post-title">'+row.title+'</div>'+
             '<div class="col datetime">'+row.datetime+'</div>'+
             '</div>' +
             '<div class="row body-post">' +
             '<div class="description">'+row.description+'</div>'+
-            '<div class="description">'+row.iduser+'</div>'+
             '</div>' +
             '</div>';
         }
