d1234e33283791b77e9f527b0fe26f6235acf4fb
Ghigolini <45860903+Ghigolini@users.noreply.github.com>
2024-06-05 19:26:33 +0200

    Update userButtonInteraction.js

--- resources/js/userButtonInteraction.js
+++ resources/js/userButtonInteraction.js
@@ -1,14 +1,11 @@
 $(document).on('click', '.savePostButton', function() {
-    // Codice per gestire il click sull'elemento
     var sellPostId = $(this).attr("value");
     
     $.ajax({
         url: "/interestlist/add",
         type: "POST",
         data: {"sellPostId": sellPostId},
-        success: function() {//response) {
-            //console.log(response);
-            // Cambiare immagine di salvataggio post e partecipazione team
+        success: function() {
             if(document.getElementById(sellPostId + "-save-post-image").value == 0) {
                 document.getElementById(sellPostId + "-save-post-image").src = "/public/saved.png";
                 document.getElementById(sellPostId + "-save-post-image").value = 1;
@@ -16,7 +13,6 @@ $(document).on('click', '.savePostButton', function() {
                 document.getElementById(sellPostId + "-save-post-image").src = "/public/save.png";
                 document.getElementById(sellPostId + "-save-post-image").value = 0;
             }
-            
         }
     });
 });
@@ -28,9 +24,7 @@ $(document).on('click', '.participateButton', function() {
         url: "/post/participate",
         type: "POST",
         data: {"teamPostId": teamPostId},
-        success: function() {//response) {
-            //console.log(response);
-            // Cambiare immagine di salvataggio post e partecipazione team
+        success: function() {
             if(document.getElementById(teamPostId + "-post-participate").value == 0) {
                 document.getElementById(teamPostId + "-post-participate").innerHTML = "Partecipi già";
                 document.getElementById(teamPostId + "-post-participate").value = 1;
@@ -38,7 +32,6 @@ $(document).on('click', '.participateButton', function() {
                 document.getElementById(teamPostId + "-post-participate").innerHTML = "Partecipa";
                 document.getElementById(teamPostId + "-post-participate").value = 0;
             }
-            
         }
     });
 });
