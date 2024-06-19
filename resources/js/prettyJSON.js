87c7c1fb3c28b39108d18e6dde89d80fa4d42b44
ghigolini <45860903+ghigolini@users.noreply.github.com>
2024-06-19 15:26:30 +0200

    Update prettyJSON.js

--- resources/js/prettyJSON.js
+++ resources/js/prettyJSON.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
     var ugly = document.getElementById('sql-result').value;
-    console.log(ugly);
-    if(ugly.length != 0 || ugly !== null || ugly.substring(0, 9) != "Exception"){
+    if(ugly.length != 0 && ugly !== null && ugly.substring(0, 9) != "Exception"){
         var obj = JSON.parse(ugly);
         var pretty = JSON.stringify(obj, undefined, 4);
         document.getElementById('sql-result').value = pretty;
